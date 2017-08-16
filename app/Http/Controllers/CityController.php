8e407d09a0896ef33e0a9748d77dfe98038b2bb4
Bryan James <bryan_james.ilaga_lds@yahoo.com>
2017-08-16 23:29:00 +0800

    Removed AJAX call for addresses that do are not yet final

--- app/Http/Controllers/CityController.php
+++ app/Http/Controllers/CityController.php
@@ -19,68 +19,47 @@ class CityController extends Controller
     }
 
     public function store(Request $r) {
-        if ($r->ajax()) {
-            if($_POST['stat']=="active") {
-                $stat = 1;
-            }
-            else if($_POST['stat']=="inactive") {
-                $stat = 0;
-            }
+        if($_POST['stat']=="active") {
+            $stat = 1;
+        }
+        else if($_POST['stat']=="inactive") {
+            $stat = 0;
+        }
 
-            if($r->chk == "checked") {
-                
-                $insertRet = City::insert(['cityName'=>$r->name,
-                                                'provinceID'=>$r->provinceID,
-                                                'archive'=>0,
-                                                'status'=>$stat]);
-                return back();
-            }
-            else {
-                
-                $insertRet = City::insert(['cityName'=>$r->name,
-                                                'archive'=>0,
-                                                'status'=>$stat]);
-                return back();
-            }
+        if($r->chk == "checked") {
+            
+            $insertRet = City::insert(['cityName'=>$r->name,
+                                            'provinceID'=>$r->provinceID,
+                                            'archive'=>0,
+                                            'status'=>$stat]);
+            return back();
         }
         else {
-            return view('errors.403');
+            
+            $insertRet = City::insert(['cityName'=>$r->name,
+                                            'archive'=>0,
+                                            'status'=>$stat]);
+            return back();
         }
     }
 
     public function getEdit(Request $r) {
-        
-        if($r->ajax()) {
-            return response(City::find($r->cityID));
-        }
-        else {
-            return view('errors.403');
-        }
+        return response(City::find($r->cityID));
     }
 
     public function edit(Request $r) {
-        if ($r->ajax()) {
-            $city = City::find($r->input('cityID'));
-            $city->cityName = $r->input('city_name');
-            $city->provinceID = $r->input('provinceID');
-            $city->status = $r->input('stat');
-            $city->save();
-            return redirect('city');
-        }
-        else {
-            return view('errors.403');
-        }
+        $city = City::find($r->input('cityID'));
+        $city->cityName = $r->input('city_name');
+        $city->provinceID = $r->input('provinceID');
+        $city->status = $r->input('stat');
+        $city->save();
+        return redirect('city');
     }
 
     public function delete(Request $r) {
-        if ($r->ajax()) {
-            $city = City::find($r->input('cityID'));
-            $city->archive = true;
-            $city->save();
-            return redirect('city');
-        }
-        else {
-            return view('errors.403');
-        }
+        $city = City::find($r->input('cityID'));
+        $city->archive = true;
+        $city->save();
+        return redirect('city');
     }
 }
