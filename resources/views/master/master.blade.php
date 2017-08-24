eb06222633d2d9562b41ccf9b5e177fea554dc01
Bryan James <bryan_james.ilaga_lds@yahoo.com>
2017-08-25 01:23:23 +0800

    Added gradient in master

--- resources/views/master/master.blade.php
+++ resources/views/master/master.blade.php
@@ -256,13 +256,13 @@
 				<!-- Start of Main Menu -->
 				<div class="main-menu menu-light menu-border menu-shadow navbar-border menu-accordion">
 					<!-- BEGIN Main Menu Header -->
-					<div class="main-menu-header bg-gradient-y2-red">
+					<div class="main-menu-header bg-gradient-y2-cyan">
 						<input type="text" placeholder="Search" class="menu-search form-control round" />
 					</div>
 					<!-- END Main Menu Header -->
 
 					<!-- BEGIN Main Menu Content -->
-					<div class="main-menu-content ">
+					<div class="main-menu-content">
 						<ul id="main-menu-navigation" data-menu="menu-navigation" class="navigation navigation-main ">
 							<li class="nav-item" id="dashboard-id">
 								<a href="/dashboard">
@@ -270,8 +270,6 @@
 									<span data-i18n="nav.dash.main" class="menu-title">Dashboard</span>
 								</a>
 							</li>
-
-							<br />
 
 							<li class="nav-item">
 								<a href="#">
